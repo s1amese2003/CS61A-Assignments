902593091f062335af6de5c41f0f46004d21ecc2
trisolaris04 <trisolaris03@gmail.com>
2025-05-16 10:02:16 +0800

    完成hw1

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i * i + j * j + k * k - max(i, j, k) * max(i, j, k)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,7 +66,9 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    for i in range(n - 1, 0, -1):
+        if n % i == 0:
+            return i
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -88,4 +90,10 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    len = 1
+    while n != 1:
+        print(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
